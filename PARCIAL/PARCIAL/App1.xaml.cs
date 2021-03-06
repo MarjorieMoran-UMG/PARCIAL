e76a3e2193b07adb3af70de15a6284488da4d1ca
MarjorieMoran-UMG <mmorann1@miumg.edu.gt>
2021-03-05 22:51:55 -0600

    PARCIAL

---
+++ PARCIAL/PARCIAL/App1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PARCIAL
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class App1 : TabbedPage
+    {
+        public App1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
